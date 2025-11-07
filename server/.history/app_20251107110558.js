af801d37e3789c5ae761913f8286a1a0cfe0e80a
abdulkareem025 <abdulaffann123gmail.com>
2025-11-07 11:21:07 +0530

    sending massage

---
+++ server/.history/app_20251107110558.js
@@ -0,0 +1,21 @@
+import express from "express"
+import fs from "fs";
+import path from "path"
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const app = express()
+const PORT = 5000;
+
+app.use(express.json());
+
+
+const buildPath = path.join(__dirname, "build");
+if (fs.existsSync(buildPath)) {
+    app.use(express.static(buildPath))
+    app.get(/^\/(?!api).*/, (req,res)=>{
+        res.se
+    })
+}
---
+++ server/.history/app_20251107111314.js
@@ -0,0 +1,40 @@
+import express from "express"
+import fs from "fs";
+import path from "path"
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const app = express()
+const PORT = 5000;
+
+app.use(express.json());
+
+
+const buildPath = path.join(__dirname, "build");
+if (fs.existsSync(buildPath)) {
+    app.use(express.static(buildPath))
+    app.get(/^\/(?!api).*/, (req,res)=>{
+        res.sendFile(path.join(buildPath, "index.html"));
+    });
+}
+
+app.get("/api/test",(req,res)=>{
+    res.json({message: "hello from /api/test!"});
+})
+
+
+
+app.use((req,res) => {
+    res.status(404).send("404- Not Found");
+});
+
+app.use((err, req ,res, next) =>{
+    console.error(err.stack);
+    res.status(500)
+})
+
+app.listen(PORT, () => {
+    console.log("Server is running on PORT ${PORT}");
+});
